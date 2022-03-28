7510c46a64ab30e9305353db41e5a9833512d6f4
danl573 <gdsryan123@gmail.com>
2022-03-28 11:38:09 -0500

    included boost library

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,46 @@
 //
 
 #include <iostream>
+#include <utility>
+#include <algorithm>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
 
-using namespace std;
 
 int main(int argc, char** argv) {
-    cout << "Hello CS3353, your arguments were: ";
-    for (int i = 1; i < argc; i++) {
-        cout << argv[i];
-        if (i < argc - 1)
-            cout << ", ";
-    }
-    cout << endl << endl;
+//    cout << "Hello CS3353, your arguments were: ";
+//    for (int i = 1; i < argc; i++) {
+//        cout << argv[i];
+//        if (i < argc - 1)
+//            cout << ", ";
+//    }
+//    cout << endl << endl;
+//
+//    return 0;
+
+
+    // create a typedef for the Graph type
+    typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS> Graph;
+
+    // Make convenient labels for the vertices
+    enum { A, B, C, D, E, N };
+    const int num_vertices = N;
+    const char* name = "ABCDE";
+
+    // writing out the edges in the graph
+    typedef std::pair<int, int> Edge;
+    Edge edge_array[] =
+            { Edge(A,B), Edge(A,D), Edge(C,A), Edge(D,C),
+              Edge(C,E), Edge(B,D), Edge(D,E) };
+    const int num_edges = sizeof(edge_array)/sizeof(edge_array[0]);
+
+    // declare a graph object
+    Graph g(num_vertices);
+
+    // add the edges to the graph object
+    for (int i = 0; i < num_edges; ++i)
+        add_edge(edge_array[i].first, edge_array[i].second, g);
 
     return 0;
 }
