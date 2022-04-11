ca025ab841b8cafea710458db7333aec07a51058
danl573 <gdsryan123@gmail.com>
2022-04-11 00:45:12 -0500

    testing with football set and formatting

--- src/Graph_helper.h
+++ src/Graph_helper.h
@@ -21,10 +21,9 @@
 #include <queue>
 
 
-struct GraphData { int community; };
+
 struct VertexProperty {
     int community = 0;
-    bool foundPaths = false;
     bool used = false;
     int distance = -1;
     int origDegree = 0;
@@ -44,10 +43,6 @@ typedef boost::adjacency_list<boost::setS, boost::vecS, boost::undirectedS, Vert
 typedef boost::range_detail::integer_iterator<unsigned long> vertexIt;
 typedef boost::graph_traits<Graph>::vertex_descriptor vd;
 
-struct Mapper {
-
-};
-
 
 class Graph_helper {
 private:
@@ -59,19 +54,69 @@ private:
     int num_communities;
 
 public:
+    // functions for graph reading and testing
     void print_graph();
     void print_edges();
     void print_vertices();
 
-    void read_graphml(const char*);
+    /**
+     * Reads the graphml into an adjacency list. Also reads one property for nodes
+     * @param file the name of the graphml file
+     */
+    void read_graphml(const char* file);
+
+    /**
+     *  sets the newDegree vertex attribute for every vertex in the current graph
+     */
     void set_degree();
+
+    /**
+     *  Calculates the modularity of the current graph and components
+     * @return the modularity
+     */
     double get_modularity();
+
+    /**
+     *  The master function for the Girvan-Newman community detection algorithm. Uses
+     *  Breadth First Search to find all shortest paths, finds edge betweeness centrality to remove
+     *  the highest used edge. For every new partition of the graph a modularity is calculated to determine
+     *  the graph's component density and outputs the most optimal cut.
+     */
     void girvan_newman();
+
+    /**
+     *  Loop for Girvan-Newman algorithm. Removes edges until new component is created, leading to a new
+     *  partition
+     */
     void girvan_newman_helper();
-    void breadth_first_search(std::map<vd, vd>&, vertexIt);
-    void reconstruct_paths(std::map<vd, vd>&, vertexIt);
-    void reset_tracking_data(vertexIt);
-    void print_report(std::vector<std::string>);
+
+    /**
+     * Implementation of Breadth First Search in order to visit the graph by visiting each adjacent node
+     * before moving further. Keeps track of each previously visited node for every node.
+     * @param prev The list of each node's previously visited node
+     * @param iter The vertex iterator for the current node
+     */
+    void breadth_first_search(std::map<vd, vd>& prev, vertexIt iter);
+
+    /**
+     * Uses prev list from BFS in order to reconstruct the shortest path for the current node to every other
+     * node. Also counts the usage of each edge
+     * @param prev The list of each node's previously visited node
+     * @param iter The vertex iterator for the current node
+     */
+    void reconstruct_paths(std::map<vd, vd>& prev, vertexIt iter);
+
+    /**
+     * Resets data used for tracking usage of vertices
+     * @param iter The vertex iterator for the current node
+     */
+    void reset_tracking_data(vertexIt iter);
+
+    /**
+     * Creates output file with community report for the graph
+     * @param report the list of vertices in each component
+     */
+    void print_report(std::vector<std::string> report) const;
 
 };
 
